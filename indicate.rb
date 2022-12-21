55881ee6a9dc07301c79a17f7fc3aab3850679aa
yuji yamada <ymdarudy@gmail.com>
2022-12-21 16:31:08 +0900

    シングルクォーテーションをダブルに自動修正

--- indicate.rb
+++ indicate.rb
@@ -1,12 +1,12 @@
 # データを受け取り、返却するCGIプログラムの起動
-require 'cgi'
+require "cgi"
 cgi = CGI.new
 
 # データを受け取った後、HTMLの形式でレスポンス（反応）を返す
-cgi.out('type' => 'text/html', 'charset' => 'UTF-8') {
+cgi.out("type" => "text/html", "charset" => "UTF-8") {
   # 受け取ったデータcgi['input']で取り出し、ローカル変数に代入する
   # 目印の'input'から情報を取り出す
-  get = cgi['input']
+  get = cgi["input"]
 
   # HTMLでレスポンスを返却する
   "<html>
